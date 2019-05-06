4723e21f5848683a635704cd70cd7ea0e348b700
Unknown <jhulsch.jh@googlemail.com>
2019-05-06 23:08:34 +0200

    stuff

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,57 @@
 <template>
-  <v-app>
-    <v-toolbar app>
+  <v-app dark>
+    <v-navigation-drawer v-model="drawer" clipped app>
+      <v-list dense>
+        <v-list-tile v-for="item in navigation.routes" :key="item.text" @click>
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.text }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-divider/>
+        <v-list-tile class="mt-3" @click>
+          <v-list-tile-action>
+            <v-icon color="grey darken-1">add_circle_outline</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title class="grey--text text--darken-1">Browse Repositories</v-list-tile-title>
+        </v-list-tile>
+        <v-list-tile @click>
+          <v-list-tile-action>
+            <v-icon color="grey darken-1">perm_contact_calendar</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title class="grey--text text--darken-1">Contact</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed clipped-left app>
+      <v-toolbar-side-icon @click.stop="drawer=!drawer"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>ANSAAL</span>
+        <span class="ml-2 font-weight-light">BUSINESS-DEVELOPMENT</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
     </v-toolbar>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <v-content></v-content>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld'
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+@Component({
+  components: {}
+})
+export default class App extends Vue {
+  name: string = "App";
+  drawer: boolean = false;
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
-    }
+
+
+  navigation = {
+    routes: [{ text: "Web-Development", icon: "web", path: "/web" }, { text: "Machine-Learning", icon: "show_chart", path: "/ml" }]
   }
+
 }
 </script>
